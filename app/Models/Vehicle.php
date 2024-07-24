a7832d87d2a7a9d52ded8cc6b1fb5ff5f1f9479c
Trevor22 <kasasatrevor25@gmail.com>
2024-07-24 09:57:59 +0300

    vehicle

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -10,20 +9,23 @@ class Vehicle extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name',
-        'number',
-        'color',
-        'model',
-        'status',
-        'customer_name',
-        'customer_contact',
-        'amount_sold',
-        'amount_paid',
-        'total_amount',
-        'balance',
-        'date_bought',
-        'period',
-        'amount_credited',
-        'monthly_deposit'
+        'name', 'number', 'color', 'model', 'status', 'customer_name', 'customer_contact',
+        'amount_sold', 'amount_paid', 'total_amount', 'balance', 'date_bought', 'period',
+        'amount_credited', 'monthly_deposit'
     ];
+
+    public function expenses()
+    {
+        return $this->hasMany(Expense::class, 'vehicle_name', 'name');
+    }
+
+    public function sales()
+    {
+        return $this->hasMany(Sale::class, 'vehicle_name', 'name');
+    }
+
+    public function installmentPlans()
+    {
+        return $this->hasMany(InstallmentPlan::class);
+    }
 }
