8ae5589dbfb50fe196a1b7a6ff6c3deb53bbd651
Trevor22 <kasasatrevor25@gmail.com>
2024-07-30 10:10:09 +0300

    rectified expenses

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Expense;
 use App\Models\Vehicle;
 use App\Models\Sale;
 use Illuminate\Http\Request;
@@ -17,7 +18,7 @@ class DashboardController extends Controller
         $pendingPayments = Sale::sum('balance'); // Assuming 'balance' represents pending payments
         $membersCount = User::count(); // Assuming you have a User model for members
         $vehiclesOwned = Vehicle::count();
-        $expenses = 45000; // Replace this with actual expense data if available
+        $expenses = Expense::sum('amount'); // Replace this with actual expense data if available
 
         // Pass the data to the view
         return view('pages.dashboard', compact('vehiclesAvailable', 'vehiclesWithBalance', 'pendingPayments', 'membersCount', 'vehiclesOwned', 'expenses'));
